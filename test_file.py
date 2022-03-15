27653101d9fa0fdb9b27c16a364663c382c318a3
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2022-03-15 11:29:54 +0100

    feat: small test file

---
+++ test_file.py
@@ -0,0 +1,8 @@
+from database import Database
+
+if __name__ == '__main__':
+    database = Database()
+    data = database.db.all()
+    last_row = data[len(data) - 1]
+    print(last_row)
+    print(len(database.db))
