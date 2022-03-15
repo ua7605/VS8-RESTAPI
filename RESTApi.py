a07739877e69fc776a81669d79e34c28d49d709a
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2022-03-15 11:44:26 +0100

    feat: connected REST API with the database.

--- RESTApi.py
+++ RESTApi.py
@@ -3,15 +3,18 @@ import json
 from flask import Flask, request
 from flask_restful import Api, Resource
 
+from database import Database
 from json_file_loader_reader import read_json_file
 
 app = Flask(__name__)
 api = Api(app)
+database = Database()
 
 
 class RetrieveVesselData(Resource):
     def get(self):
-        return read_json_file(file_name="data.json")
+        return database.latest_value()
+        # return read_json_file(file_name="data.json")
 
 
 class HelloWorld(Resource):
