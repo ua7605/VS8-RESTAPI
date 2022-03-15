82ac8eb0781d9209d72ffdc3782f4b5c3a4dcdae
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2022-03-15 15:32:06 +0100

    feat: made a small script how you need to read out back the "historicalData".

--- test_file.py
+++ test_file.py
@@ -1,5 +1,7 @@
 import json
 
+import requests
+
 from database import Database
 
 if __name__ == '__main__':
@@ -15,12 +17,24 @@ if __name__ == '__main__':
     #print(database.get_vessel_current_location())
     #print(database.get_vessel_historical_data())
     json_string = database.get_vessel_historical_data()#str(database.get_vessel_historical_data()).replace('[', '{', 1).replace(']', '}', 1)
-    print(json_string)
+    #print(json_string)
     # print(json_string)
     #normal_obj = json.loads(json_string)
     json_obj = json.dumps(json_string)
-    print(json_obj)
-    text:str = "hello"
+    #print(json_obj)
+
+
+
+
+    """
+        this is how you need to read the "historicalData" data back in
+    """
+    print("This is how you need to read back in the data you retrieve from historicalData")
+    response = requests.get("http://193.190.127.147:5000/data/historicalData")
+    data = response.json()
+    json_data = json.loads(data)
+    print(json_data[len(json_data) - 1 ])
+
 
 
 
