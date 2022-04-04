b869fafa2f8b2904f251161e1c99952ba5dca810
Vincent Charpentier <vincent.charpentier@uantwerpen.be>
2022-04-04 09:48:44 +0200

    style: renamed a function to make the purpose clearer.

--- mainVirtualWall.py
+++ mainVirtualWall.py
@@ -5,15 +5,15 @@ from RESTApi import app
 from zmq_subscriber import ZMQSubscriberSeafar
 
 
-def start_REST_API():
+def start_subscriber_publisher_service():
     # app.run(host="193.190.127.147", debug=True)
-    subscriber_seafar = ZMQSubscriberSeafar()
+    ZMQSubscriberSeafar()
     print("started")
 
 
 if __name__ == '__main__':
     # This is the main you need to start at the Virtual Wall!!!
     # subscriber_seafar = ZMQSubscriberSeafar()
-    thread = threading.Thread(target=start_REST_API)
+    thread = threading.Thread(target=start_subscriber_publisher_service)
     thread.start()
     app.run(host="193.190.127.147", debug=True)
